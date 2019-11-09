83a2a080bfd35c8d3c0b2c0f809e1293a8a944a5
Harkunwar Singh Kochar <geek@harkunwar.com>
2019-11-09 01:12:59 -0800

    LTA-18 common class imports

--- lib/firecross.dart
+++ lib/firecross.dart
@@ -2,3 +2,6 @@ library firecross;
 
 export 'package:firecross/firecross_auth.dart';
 export 'package:firecross/firecross_app.dart';
+
+export 'package:firecross/src/common/firecross_user.dart';
+export 'package:firecross/src/common/firecross_auth_result.dart';
